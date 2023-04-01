8e6853c578e205f9ba7f2fd90d8a6fe2f019a422
asg <sg.andre.dados1@gmail.com>
2023-04-01 16:11:29 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+text = """Content"""
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt=text,
+  temperature=0,
+  max_tokens=100,
+  top_p=1.0,
+  frequency_penalty=0.0,
+  presence_penalty=0.0
+)
